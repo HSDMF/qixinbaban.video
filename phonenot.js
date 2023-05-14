0af32a4144fb2432f96e2d7857c65ae82012145e
HSDMF <98017956+HSDMF@users.noreply.github.com>
2023-05-14 20:17:43 +0800

    Add files via upload

--- phonenot.js
+++ phonenot.js
@@ -1,5 +1,5 @@
 if((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
     window.location.href = "index.mobile.html";     //手机
 } else {
-    window.location.href = "23Spvideo.html";        //电脑
+    window.location.href = "indexhtml.html";        //电脑
 }
